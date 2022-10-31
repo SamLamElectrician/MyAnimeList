3796badb91475c077d0ab8a03e31fff0c25e47f7
Sammy <sammylam_1@hotmail.com>
2022-10-31 10:36:01 -0400

    mistakes

--- src/App.js
+++ src/App.js
@@ -4,24 +4,20 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './component/Header';
 import Main from './component/Main';
 import About from './component/About';
-// import Signup from './component/Signup';
-// import Login from './component/Login';
-import { UserAuthContextProvider } from './context/UserAuthContext';
 
 function App() {
 	return (
 		<>
 			{/* <Header /> */}
-			<UserAuthContextProvider>
-				<Header />
-				<Routes>
-					{/* <Route exact path='/' element={<Login />} /> */}
-					{/* <Route path='/signup' element={<Signup />} /> */}
 
-					<Route exact path='/' element={<About />}></Route>
-					<Route path='/home' element={<Main />}></Route>
-				</Routes>
-			</UserAuthContextProvider>
+			<Header />
+			<Routes>
+				{/* <Route exact path='/' element={<Login />} /> */}
+				{/* <Route path='/signup' element={<Signup />} /> */}
+
+				<Route exact path='/' element={<About />}></Route>
+				<Route path='/home' element={<Main />}></Route>
+			</Routes>
 		</>
 	);
 }
