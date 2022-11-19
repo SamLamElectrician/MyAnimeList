f58ae18679feab82d9ed6337ff0df7fc84722b88
SammyLam <sammylam_1@hotmail.com>
2022-11-18 20:02:34 -0500

    loading

--- src/component/AnimeCard.js
+++ src/component/AnimeCard.js
@@ -1,33 +1,27 @@
 import React from 'react';
-import loading from '../assets/images/loading.png';
 
 const AnimeCard = ({ anime }) => {
 	//takes data from Main api call to return a card
-
-	if (anime) {
-		return (
-			<article className='animeCard'>
-				<a href={anime.url} target='_blank' rel='noreferrer'>
-					<figure>
-						<img src={anime.images.jpg.large_image_url} alt='anime' />
-					</figure>
-					<div className='info'>
-						<h3>
-							{anime.title} ||<br></br> {anime.title_japanese}
-						</h3>
-						<p>{anime.synopsis}</p>
-					</div>
-				</a>
-				{/* working on button */}
-				<button buttonKey={anime.mal_id}>
-					{/* <button buttonId={anime.mal_id} onClick={AddToList}> */}
-					Add To List
-				</button>
-			</article>
-		);
-	} else {
-		return <img src={loading} alt='loading icon'></img>;
-	}
+	return (
+		<article className='animeCard'>
+			<a href={anime.url} target='_blank' rel='noreferrer'>
+				<figure>
+					<img src={anime.images.jpg.large_image_url} alt='anime' />
+				</figure>
+				<div className='info'>
+					<h3>
+						{anime.title} ||<br></br> {anime.title_japanese}
+					</h3>
+					<p>{anime.synopsis}</p>
+				</div>
+			</a>
+			{/* working on button */}
+			<button buttonKey={anime.mal_id}>
+				{/* <button buttonId={anime.mal_id} onClick={AddToList}> */}
+				Add To List
+			</button>
+		</article>
+	);
 };
 
 export default AnimeCard;
